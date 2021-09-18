caebe970dc22b8b0841a4c7c7589afcfdfc95951
Peter Hull <peterhull90@gmail.com>
2021-09-18 09:54:17 +0100

    ex_windows: moved to named positions as well as random
    This should show more clearly if positioning is incorrect

--- examples/ex_windows.c
+++ examples/ex_windows.c
@@ -9,10 +9,79 @@
 const int W = 100;
 const int H = 100;
 
-
+/* Cycle through some different named positions for the windows */
+static void make_position(ALLEGRO_MONITOR_INFO *info, int *px, int *py, const char **caption)
+{
+   static int type = 0;
+   int x, y;
+   int monitor_w, monitor_h;
+   x = info->x1;
+   y = info->y1;
+   monitor_w = info->x2 - info->x1;
+   monitor_h = info->y2 - info->y1;
+   switch (type++ % 16) {
+      case 0:
+      case 4:
+      case 8:
+      case 12:
+         *caption = "Center";
+         *px = x + (monitor_w - W) / 2;
+         *py = y + (monitor_h - H) / 2;
+         break;
+      case 1:
+         *caption = "Edge";
+         *px = x + (monitor_w - W) / 2;
+         *py = y;
+         break;
+      case 5:
+         *caption = "Edge";
+         *px = x + (monitor_w - W);
+         *py = y + (monitor_h - H) / 2;
+         break;
+      case 9:
+         *caption = "Edge";
+         *px = x + (monitor_w - W) / 2;
+         *py = y + (monitor_h - H);
+         break;
+      case 13:
+         *caption = "Edge";
+         *px = x;
+         *py = y + (monitor_h - H) / 2;
+         break;
+      case 2:
+         *caption = "Thirds";
+         *px = x + (monitor_w - W) / 3;
+         *py = y + (monitor_h - H) / 3;
+      break;
+      case 6:
+         *caption = "Thirds";
+         *px = x + (monitor_w - W) * 2 / 3;
+         *py = y + (monitor_h - H) / 3;
+      break;
+      case 10:
+         *caption = "Thirds";
+         *px = x + (monitor_w - W) / 3;
+         *py = y + (monitor_h - H) * 2 / 3;
+      break;
+      case 14:
+         *caption = "Thirds";
+         *px = x + (monitor_w - W) * 2 / 3;
+         *py = y + (monitor_h - H) * 2 / 3;
+      break;
+      case 3:
+      case 7:
+      case 11:
+      case 15:
+         *caption = "Random";
+         *px = x + rand() % (monitor_w - W);
+         *py = y + rand() % (monitor_h - H);
+         break;
+   }
+}
 int main(int argc, char **argv)
 {
    ALLEGRO_DISPLAY *displays[2];
+   const char* captions[2];
    ALLEGRO_MONITOR_INFO *info;
    int adapter_count;
    int x, y;
@@ -52,12 +121,12 @@ int main(int argc, char **argv)
    al_set_new_window_position(x, y);
 
    displays[0] = al_create_display(W, H);
-
+   captions[0] = "Click me...";
    x *= 2;
    al_set_new_window_position(x, y);
 
    displays[1] = al_create_display(W, H);
-
+   captions[1] = "Click me...";
    if (!displays[0] || !displays[1]) {
       abort_example("Could not create displays.\n");
    }
@@ -82,7 +151,7 @@ int main(int argc, char **argv)
         else
            al_clear_to_color(al_map_rgb(155, 255, 0));
         al_set_blender(ALLEGRO_ADD, ALLEGRO_ONE, ALLEGRO_INVERSE_ALPHA);
-        al_draw_textf(myfont, al_map_rgb(0, 0, 0), 50, 50, ALLEGRO_ALIGN_CENTRE, "Click me..");
+        al_draw_textf(myfont, al_map_rgb(0, 0, 0), 50, 50, ALLEGRO_ALIGN_CENTRE, "%s", captions[i]);
         al_flip_display();
       }
 
@@ -92,11 +161,8 @@ int main(int argc, char **argv)
          }
          else if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
             int a = rand() % adapter_count;
-            int w = info[a].x2 - info[a].x1;
-            int h = info[a].y2 - info[a].y1;
-            int margin = 20;
-            x = margin + info[a].x1 + (rand() % (w - W - margin));
-            y = margin + info[a].y1 + (rand() % (h - H - margin));
+            int d = event.mouse.display == displays[0] ? 0 : 1;
+            make_position(&info[a], &x, &y, &captions[d]);
             al_set_window_position(event.mouse.display, x, y);
          }
       }
